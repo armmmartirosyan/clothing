751afe8204221b7e6582e0f701ebb6090953386a
armmmartirosyan <armenmartirosyan020@gmail.com>
2024-04-19 01:08:30 +0400

    used env variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 
 import axios from "axios";
-import { useHost } from "@/hooks/useHost";
+
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default function Home() {
-  const host = useHost();
-
   const handleClick = () => {
-    if (!host) {
-      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-      return;
-    }
-
     axios
-      .get(`${host}api/init`)
+      .get(`${baseUrl}api/init`)
       .then((response) => {
         console.log(response, "-----------SUCCESS-----------");
       })
